3131ae3e6b6a091a47dfddad2cd48e7b1dd876f3
Mirakurun <25449974+Mirakurun@users.noreply.github.com>
2019-07-30 22:01:59 -0700

    Add chalk success console.log

--- backend/express/db/mongoose.js
+++ backend/express/db/mongoose.js
@@ -1,11 +1,14 @@
 const mongoose = require('mongoose');
+const chalk = require('chalk');
+
+const success = chalk.green;
 
 mongoose
   .connect(process.env.DB_URI, {
     useNewUrlParser: true,
     useCreateIndex: true,
   })
-  .then(() => console.log('Mongoose connection succeeded.'))
+  .then(() => console.log(success('Mongoose connection succeeded.')))
   .catch(error => console.error(error));
 
 module.exports = mongoose;
--- backend/express/server.js
+++ backend/express/server.js
@@ -1,5 +1,9 @@
+const chalk = require('chalk');
 const app = require('./app');
 
+const success = chalk.green;
 const port = process.env.PORT;
 
-app.listen(port, () => console.log(`Server is running on port: ${port}`));
+app.listen(port, () =>
+  console.log(success(`Server is running on port: ${port}`))
+);
