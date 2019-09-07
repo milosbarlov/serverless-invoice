066936386e14c867c02bc64116fd310f7016832f
Mirakurun <25449974+Mirakurun@users.noreply.github.com>
2019-09-06 18:27:30 -0700

    delete boot and mixin files

--- src/boot/currency-formatter.js
+++
@@ -1,5 +0,0 @@
-import currencyFormatter from 'currency-formatter';
-
-export default async ({ Vue }) => {
-  Vue.prototype.$currencyFormatter = currencyFormatter;
-};
--- src/boot/moment.js
+++
@@ -1,5 +0,0 @@
-import moment from 'moment';
-
-export default async ({ Vue }) => {
-  Vue.prototype.$moment = moment;
-};
