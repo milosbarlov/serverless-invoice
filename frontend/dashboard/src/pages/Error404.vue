0d0db1dfb87131379f97419e326b8fa3d8c87b58
Mirakurun <25449974+Mirakurun@users.noreply.github.com>
2020-01-25 22:28:01 -0800

    Update Error404.vue

--- frontend/dashboard/src/pages/Error404.vue
+++ frontend/dashboard/src/pages/Error404.vue
@@ -1,17 +1,19 @@
 <template>
-  <q-layout style="background-color:black;">
-    <div class="fixed-center text-center">
-      <p>
-        <img
-          src="statics/img/smug-anime-face.png"
-          xstyle="width:30vw;max-width:150px;"
-          class="fit"
-        />
-      </p>
-      <p class="text-faded">Sorry, nothing here... <strong>(404)</strong></p>
-      <q-btn color="secondary" style="width:200px;" @click="$router.push('/')"
-        >Go Home</q-btn
-      >
-    </div>
+  <q-layout style="background-color: black">
+    <q-page-container>
+      <q-page class="flex flex-center">
+        <div class="block">
+          <p class="text-white text-center">
+            Sorry, nothing here... <strong>(404)</strong>
+          </p>
+          <q-btn
+            color="secondary"
+            style="width:200px;"
+            @click="$router.push('/')"
+            >Go Home</q-btn
+          >
+        </div>
+      </q-page>
+    </q-page-container>
   </q-layout>
 </template>
