112c5a0fd2c6c027478d59025360165a6aef4877
Mirakurun <25449974+Mirakurun@users.noreply.github.com>
2020-01-25 22:02:31 -0800

    Add boot files

---
+++ frontend/dashboard/src/boot/fontawesome.js
@@ -0,0 +1,9 @@
+import '@fortawesome/fontawesome-free/css/fontawesome.min.css';
+import '@fortawesome/fontawesome-free/css/brands.min.css';
+import '@fortawesome/fontawesome-free/css/regular.min.css';
+import '@fortawesome/fontawesome-free/css/solid.min.css';
+
+// leave the export, even if you don't use it
+export default () => {
+  // something to do
+};
