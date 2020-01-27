8bd296b7355d6fe9ca8db8f7eb2b0d36ced848d1
Mirakurun <25449974+Mirakurun@users.noreply.github.com>
2020-01-26 23:21:47 -0800

    Add mongoose connection file

--- backend/express/db/mongoose.js
+++ backend/express/db/mongoose.js
@@ -7,6 +7,7 @@ mongoose
   .connect(process.env.DB_URI, {
     useNewUrlParser: true,
     useCreateIndex: true,
+    useFindAndModify: false,
   })
   .then(() => console.log(success('Mongoose connection succeeded.')))
   .catch(error => console.error(error));
