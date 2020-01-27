d12ef6ded7a1cfd06190bdbce64d1c7eac446e21
Mirakurun <25449974+Mirakurun@users.noreply.github.com>
2020-01-26 23:34:13 -0800

    Add root files

--- backend/express/.eslintrc.js
+++ backend/express/.eslintrc.js
@@ -3,7 +3,8 @@ module.exports = {
     es6: true,
     node: true,
   },
-  extends: ['eslint:recommended', 'plugin:node/recommended'],
+  extends: ['eslint:recommended', 'plugin:node/recommended', 'plugin:prettier/recommended', 'plugin:jest/recommended'],
+  plugins: ['prettier', 'jest'],
   globals: {
     Atomics: 'readonly',
     SharedArrayBuffer: 'readonly',
@@ -15,6 +16,8 @@ module.exports = {
   rules: {
     'node/no-unsupported-features/es-syntax': 'off',
     'node/no-unpublished-require': 'off',
-    'no-console': 'off'
+    'no-console': 'off',
+
+    'prettier/prettier': ['error', { singleQuote: true, trailingComma: 'es5' }],
   },
 };
--- backend/express/app.js
+++ backend/express/app.js
@@ -5,23 +5,36 @@ const { mongoose } = require('./db/mongoose');
 const customersRouter = require('./routes/customers');
 const paymentsRouter = require('./routes/payments');
 const invoicesRouter = require('./routes/invoices');
+const settingsRouter = require('./routes/settings');
+const authRouter = require('./routes/auth');
+const checkoutRouter = require('./routes/checkout');
+const chartRouter = require('./routes/chart');
 
 const app = express();
 
+// Maintenance mode middleware
+// app.use((req, res, next) => {
+//   res.status(503).send('Website under maintenance!');
+// });
+
 app.use(cors());
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: true }));
 
 app.use('/customers', customersRouter);
 app.use('/payments', paymentsRouter);
 app.use('/invoices', invoicesRouter);
+app.use('/settings', settingsRouter);
+app.use('/auth', authRouter);
+app.use('/checkout', checkoutRouter);
+app.use('/chart', chartRouter);
 
 // Express error handler middleware
 app.use((error, req, res, next) => {
   console.error(error);
   const status = error.statusCode || 500;
-  const { message } = error;
-  res.status(status).json({ message });
+  const { message, data } = error;
+  res.status(status).json({ message, data });
 });
 
 module.exports = app;
