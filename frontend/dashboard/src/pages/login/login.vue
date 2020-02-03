00f6273178b81b4bcf2bfbfbdf8e835ab5de189e
Mirakurun <25449974+Mirakurun@users.noreply.github.com>
2020-02-02 17:46:48 -0800

    Add form submit handler

--- frontend/dashboard/src/pages/login/login.vue
+++ frontend/dashboard/src/pages/login/login.vue
@@ -1,74 +1,76 @@
 <template>
   <q-page class="flex flex-center" :padding="!$q.platform.is.mobile">
-    <q-card
-      :class="{
-        'text-2b': true,
-        'full-width': $q.platform.is.mobile || $q.screen.lt.sm,
-        'window-height': $q.platform.is.mobile || $q.screen.lt.sm,
-        'w-500px': !$q.platform.is.mobile,
-      }"
-      :flat="$q.platform.is.mobile"
-    >
-      <q-card-section>
-        <div class="text-h4 text-center q-my-md">Log In</div>
-      </q-card-section>
-      <q-card-section>
-        <div class="q-gutter-y-md">
-          <alert
-            :alert="$store.state.auth.error || error ? true : false"
-            :message="$store.state.auth.error || error"
+    <form @submit.prevent="login">
+      <q-card
+        :class="{
+          'text-2b': true,
+          'full-width': $q.platform.is.mobile || $q.screen.lt.sm,
+          'window-height': $q.platform.is.mobile || $q.screen.lt.sm,
+          'w-500px': !$q.platform.is.mobile,
+        }"
+        :flat="$q.platform.is.mobile"
+      >
+        <q-card-section>
+          <div class="text-h4 text-center q-my-md">Log In</div>
+        </q-card-section>
+        <q-card-section>
+          <div class="q-gutter-y-md">
+            <alert
+              :alert="$store.state.auth.error || error ? true : false"
+              :message="$store.state.auth.error || error"
+            />
+            <q-input
+              color="cyan"
+              filled
+              :error="$v.form.username.$error"
+              error-message="Enter a username"
+              label="Username"
+              v-model="form.username"
+            >
+              <template #prepend>
+                <q-icon name="fas fa-fw fa-sm fa-user" />
+              </template>
+            </q-input>
+            <q-input
+              color="cyan"
+              filled
+              :error="$v.form.password.$error"
+              error-message="Enter a password"
+              label="Password"
+              :type="isPwd ? 'password' : 'text'"
+              v-model="form.password"
+            >
+              <template #prepend>
+                <q-icon name="fas fa-fw fa-sm fa-key" />
+              </template>
+              <template #append>
+                <q-icon
+                  class="cursor-pointer"
+                  @click="isPwd = !isPwd"
+                  :name="isPwd ? 'visibility_off' : 'visibility'"
+                >
+                  <q-tooltip>
+                    {{ isPwd ? 'Show password' : 'Hide password' }}
+                  </q-tooltip>
+                </q-icon>
+              </template>
+            </q-input>
+          </div>
+        </q-card-section>
+        <q-card-actions class="q-pa-md">
+          <q-btn
+            class="q-my-md full-width"
+            @click="login"
+            color="cyan"
+            label="Log In"
+            no-caps
+            rounded
+            size="lg"
+            type="submit"
           />
-          <q-input
-            color="cyan"
-            filled
-            :error="$v.form.username.$error"
-            error-message="Enter a username"
-            label="Username"
-            v-model="form.username"
-          >
-            <template #prepend>
-              <q-icon name="fas fa-fw fa-sm fa-user" />
-            </template>
-          </q-input>
-          <q-input
-            color="cyan"
-            filled
-            :error="$v.form.password.$error"
-            error-message="Enter a password"
-            label="Password"
-            :type="isPwd ? 'password' : 'text'"
-            v-model="form.password"
-          >
-            <template #prepend>
-              <q-icon name="fas fa-fw fa-sm fa-key" />
-            </template>
-            <template #append>
-              <q-icon
-                class="cursor-pointer"
-                @click="isPwd = !isPwd"
-                :name="isPwd ? 'visibility_off' : 'visibility'"
-              >
-                <q-tooltip>
-                  {{ isPwd ? 'Show password' : 'Hide password' }}
-                </q-tooltip>
-              </q-icon>
-            </template>
-          </q-input>
-        </div>
-      </q-card-section>
-      <q-card-actions class="q-pa-md">
-        <q-btn
-          class="q-my-md full-width"
-          @click="login"
-          color="cyan"
-          label="Log In"
-          no-caps
-          rounded
-          size="lg"
-          type="submit"
-        />
-      </q-card-actions>
-    </q-card>
+        </q-card-actions>
+      </q-card>
+    </form>
   </q-page>
 </template>
 
